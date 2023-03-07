c87d3bd54ab47e4c20114e96f1139f98f3e44781
Sarah mohamed <sara20191700278@cis.asu.edu.eg>
2023-03-07 13:19:12 +0200

    Encrypt Playfair

--- securitylibrary/MainAlgorithms/PlayFair.cs
+++ securitylibrary/MainAlgorithms/PlayFair.cs
@@ -13,9 +13,43 @@ namespace SecurityLibrary
             throw new NotImplementedException();
         }
 
+
         public string Encrypt(string plainText, string key)
         {
-            throw new NotImplementedException();
+            //check conditions of plain Text(duplicated,length)
+            for(int i = 0; i < plainText.Length - 1; i += 2)
+            {
+                if (plainText[i] == plainText[i + 1])
+                {
+                    plainText = plainText.Substring(0, i + 1) + 'x' + plainText.Substring(i + 1);
+
+                }
+            }
+            if (plainText.Length % 2 != 0) plainText += 'x';
+            ///////////initialize key
+            HashSet<char> Table = new HashSet<char>();
+            string alpha= "abcdefghjklmnopqrstuvwxyz";//put j instead of i
+            for(int i = 0; i < key.Length; i++)
+            {
+                if (key[i] == 'i')
+                {
+                    Table.Add('j');
+                }
+                else
+                {
+                    Table.Add(key[i]);
+                }
+
+            }
+            //continue the table
+
+            for(int i = 0; i < 25; i++)
+            {
+                Table.Add(alpha[i]);
+            }
+
+
+
         }
     }
 }
