4c69c03c92a7883feb7722932cf760de63b14af5
AdHam14a <95590371+AdHam14a@users.noreply.github.com>
2023-03-11 18:08:03 +0200

    Ceaser and Mono are done

--- securitylibrary/MainAlgorithms/Ceaser.cs
+++ securitylibrary/MainAlgorithms/Ceaser.cs
@@ -8,19 +8,68 @@ namespace SecurityLibrary
 {
     public class Ceaser : ICryptographicTechnique<string, int>
     {
+        public static char cipherCharacter(char ch, int key)
+        {
+            char offset = char.IsUpper(ch) ? 'A' : 'a';
+            char newCh = (char)((((ch + key) - offset) % 26) + offset);
+            return newCh;
+        }
+
+        char[] characters = new char[25];
+        string[] words = new string[25];
+
         public string Encrypt(string plainText, int key)
         {
-            throw new NotImplementedException();
+            Console.WriteLine("The encryption class : ");
+            string output = "";
+            foreach (char ch in plainText)
+            {                
+                output += cipherCharacter(ch,key);
+            }
+
+            
+
+            Console.WriteLine("The text is : " + output);
+            return output;
+            //throw new NotImplementedException();
         }
+
+        string[] character = new string[26];
 
         public string Decrypt(string cipherText, int key)
         {
-            throw new NotImplementedException();
+            Console.WriteLine("The decryption class : ");
+            return Encrypt(cipherText, 26 - key);
+            //throw new NotImplementedException();
         }
 
         public int Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+            Console.WriteLine("The analysis class : ");
+            int key = 0;
+            string resultPlain = Encrypt(plainText, key);
+
+            Console.WriteLine("Real inputs : ");
+            Console.WriteLine(cipherText);
+            Console.WriteLine("My inputs");
+            Console.WriteLine(resultPlain);
+            Console.WriteLine("----------------------");
+            Console.WriteLine("The loop is running");
+            Console.WriteLine("----------------------");
+
+
+            while (resultPlain != cipherText.ToLower() && key < 26)
+            {
+                key++;
+                resultPlain = Encrypt(plainText, key);
+                Console.WriteLine(resultPlain);
+                Console.WriteLine("---------------");
+            }
+            Console.WriteLine(key);
+            return key;
+            
+
+            //throw new NotImplementedException();
         }
     }
 }
--- securitylibrary/MainAlgorithms/Monoalphabetic.cs
+++ securitylibrary/MainAlgorithms/Monoalphabetic.cs
@@ -8,19 +8,86 @@ namespace SecurityLibrary
 {
     public class Monoalphabetic : ICryptographicTechnique<string, string>
     {
+        
         public string Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+            Console.WriteLine("The analysis class : ");
+            cipherText = cipherText.ToLower();
+            Dictionary<Char, Char> dict = new Dictionary<char, char>();
+            char sValue = 'a';
+            char cValue = 'A';
+            for (int i = 0; i < 26; i++)
+            {
+
+                dict[sValue] = cValue;
+                cValue++;
+                sValue++;
+            }
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                dict[plainText[i]] = cipherText[i];
+            }
+            string key = "";
+
+            var keysList = dict.Keys.ToList();
+            keysList.Sort();
+
+            foreach (var keys in keysList)
+            {
+                key += dict[keys];
+            }
+            Console.Write("The key is : "); Console.Write(key);
+            return key;
+            //throw new NotImplementedException();
         }
 
         public string Decrypt(string cipherText, string key)
         {
-            throw new NotImplementedException();
+            string[] encr = new string[26];
+            Console.WriteLine("The decryption class : ");
+            string decryptedWord = "";
+            char character = 'a';
+            cipherText = cipherText.ToLower();
+            Dictionary<Char, Char> dict = new Dictionary<char, char>();
+            for (int i = 0; i < key.Length; i++)
+            {
+                dict.Add(key[i], character);
+                //Console.Write(x);
+                character++;
+            }
+            for (int i = 0; i < cipherText.Length; i++)
+            {
+                char val = cipherText[i];
+                decryptedWord += dict[val];
+            }
+            Console.Write("The encrypted word : "); Console.Write(cipherText);
+            Console.Write("\nThe real word : "); Console.Write(decryptedWord);
+            
+            return decryptedWord;
+            //throw new NotImplementedException();
         }
 
         public string Encrypt(string plainText, string key)
         {
-            throw new NotImplementedException();
+            string[] decr = new string[26];
+            Console.WriteLine("The encryption class : ");
+            string encryptedWord = "";
+            char character = 'a';
+            Dictionary<Char, Char> mp = new Dictionary<char, char>();
+            for (int i = 0; i < key.Length; i++)
+            {
+                mp.Add(character, key[i]);
+                character++;
+            }
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                char value = plainText[i];
+                encryptedWord += mp[value];
+            }
+            Console.Write("The real word : "); Console.Write(plainText);
+            Console.Write("\nThe encrypted word : "); Console.Write(encryptedWord);
+            return encryptedWord;
+            //throw new NotImplementedException();
         }
 
         /// <summary>
@@ -56,7 +123,64 @@ namespace SecurityLibrary
         /// <returns>Plain text</returns>
         public string AnalyseUsingCharFrequency(string cipher)
         {
-            throw new NotImplementedException();
+            string[] characters = new string[26];
+            string word = "";
+            Dictionary<int, char> dict = new Dictionary<int, char>();
+            dict.Add(1, 'E');
+            dict.Add(2, 'T');
+            dict.Add(3, 'A');
+            dict.Add(4, 'O');
+            dict.Add(5, 'I');
+            dict.Add(6, 'N');
+            dict.Add(7, 'S');
+            dict.Add(8, 'R');
+            dict.Add(9, 'H');
+            dict.Add(10, 'L');
+            dict.Add(11, 'D');
+            dict.Add(12, 'C');
+            dict.Add(13, 'U');
+            dict.Add(14, 'M');
+            dict.Add(15, 'F');
+            dict.Add(16, 'P');
+            dict.Add(17, 'G');
+            dict.Add(18, 'W');
+            dict.Add(19, 'Y');
+            dict.Add(20, 'B');
+            dict.Add(21, 'V');
+            dict.Add(22, 'K');
+            dict.Add(23, 'X');
+            dict.Add(24, 'J');
+            dict.Add(25, 'Q');
+            dict.Add(26, 'Z');
+            Dictionary<Char, int> count_dict = new Dictionary<char, int>();
+            for (char a = 'A'; a <= 'Z'; a++)
+            {
+                count_dict.Add(a, 0);
+            }
+            for (int i = 0; i < cipher.Length; i++)
+            {
+                count_dict[cipher[i]]++;
+            }
+
+            var items = from pair in count_dict
+                        orderby pair.Value descending
+                        select pair;
+            int n = 1;
+            Dictionary<char, char> keyMap = new Dictionary<char, char>();
+            // keymap key=> chiper, value=> main cahr 
+            foreach (KeyValuePair<char, int> pair in items)
+            {
+                keyMap.Add(pair.Key, dict[n]);
+                n++;
+            }
+            for (int i = 0; i < cipher.Length; i++)
+            {
+                word += keyMap[cipher[i]];
+            }
+
+            Console.Write("The word : "); Console.Write(word);
+            return word;
+            //throw new NotImplementedException();
         }
     }
 }
